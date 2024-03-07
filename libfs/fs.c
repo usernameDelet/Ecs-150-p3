f401619d34f97c1626b38db495c80c34b4b99c40
Zhuo Chen <91066953+cckda@users.noreply.github.com>
2024-03-07 11:38:22 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -121,17 +121,23 @@ int fs_info(void)
     printf("data_blk_count=%u\n", super.amount_data_blocks);
     printf("fat_free_ratio=%u/%u\n", super.amount_data_blocks - super.num_blocks_fat, super.amount_data_blocks);
 
-    int rdirCount = 0;
-    for(int i = 0; i < FS_FILE_MAX_COUNT; i++)
-    {
-        if(strcmp(rootDir[i].filename, "\0") == 0)
-        {
-            rdirCount = rdirCount + 1;
+    int fat_free_blocks = 0;
+    for (int i = 0; i < super.amount_data_blocks; i++) {
+        if (fat[i] == 0) {
+            fat_free_blocks++;
         }
     }
-    printf("rdir_free_ratio=%d/%d\n", rdirCount, FS_FILE_MAX_COUNT);
-    
-	return SUCCE;
+    printf("fat_free_ratio=%d/%u\n", fat_free_blocks, super.amount_data_blocks);
+
+    int rdir_free_entries = 0;
+    for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+        if (rootDir[i].filename[0] == '\0') {
+            rdir_free_entries++;
+        }
+    }
+    printf("rdir_free_ratio=%d/%d\n", rdir_free_entries, FS_FILE_MAX_COUNT);
+
+    return SUCCESS;
 }
 
 int fs_create(const char *filename)
