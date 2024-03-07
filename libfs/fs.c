deac22073a30cf0d03e42195b9952d264d74bfbf
Shijie Wei <112788068+usernameDelet@users.noreply.github.com>
2024-03-07 00:07:47 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -51,38 +51,39 @@ int fs_mount(const char *diskname)
     fat = malloc(sizeof(uint16_t) * super.num_blocks_fat * BLOCK_SIZE);
     if(block_disk_open(diskname) == ERROR)
     {
+        printf("e 1");
         return ERROR;
     }
     if (strncmp(super.signature, "ECS150FS", 8) != 0)
     {
-       
+       printf("e 2");
         return ERROR;
     }
     if(block_read(0, &super) == ERROR)
     {
-        
+        printf("e 3");
         return ERROR;
     }
     if(super.total_blocks != block_disk_count())
     {
-        
+        printf("e 4");
         return ERROR;
     }
     
     if (block_read(super.root_dir_index, (void *)rootDir) == ERROR) 
     {
-       
+       printf("e 5");
         return ERROR;
     }
     for(int i = 0; i < super.num_blocks_fat; i++) 
     {
         if(block_read(i+1, &fat[i]) == ERROR)
         {
-            
+            printf("e 6");
             return ERROR;
         }
     }
-    
+    printf("done");
     return SUCCE;
 }
 
