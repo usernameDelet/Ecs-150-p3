477b1541c463be54fea56e01763a909f2061c3aa
Shijie Wei <112788068+usernameDelet@users.noreply.github.com>
2024-03-08 23:49:54 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -3,14 +3,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-
 #include "disk.h"
 #include "fs.h"
-
 #define ERROR -1
 #define SUCCE 0
 #define FAT_EOC 0xFFFF
-
 /* TODO: Phase 1 */
 struct superblock 
 {
@@ -22,7 +19,6 @@ struct superblock
     uint8_t  num_blocks_fat;       
     uint8_t  padding[4079];         
 }__attribute__((packed));;
-
 struct rootDirectory
 {
 	char 		filename[FS_FILENAME_LEN];
@@ -30,85 +26,93 @@ struct rootDirectory
 	uint16_t 	index_of_first;
 	uint8_t 	unused[10];
 };
-
 struct file_descriptor
 {
 	char  filename[FS_FILENAME_LEN];
 	int   file_index;
 	int   offset;
 };
-
 uint16_t *fat;
 struct superblock super;
 struct rootDirectory rootDir[FS_FILE_MAX_COUNT];
 struct file_descriptor filed[FS_OPEN_MAX_COUNT];
-
 int fs_mount(const char *diskname)
 {
     /* TODO: Phase 1 */
+    // error checking
     if(block_disk_open(diskname) == ERROR)
     {
+        // no disk opened
         return ERROR;
     }
     if(block_read(0, &super) == ERROR)
     {
+        // fail to read the block
         return ERROR;
     }
     if (strncmp(super.signature, "ECS150FS", 8) != 0)
     {
+        // the signatura does not match 
         return ERROR;
     }
     if(super.total_blocks != block_disk_count())
     {
+        // the opened disk number does not match
         return ERROR;
     }
     if (block_read(super.root_dir_index, (void *)rootDir) == ERROR) 
     {
+        // fail to read the block
         return ERROR;
     }
+    // allocate new memory for fat for block reading 
     fat = malloc(sizeof(uint16_t) * super.num_blocks_fat * BLOCK_SIZE);
     for(int i = 0; i < super.num_blocks_fat; i++) 
     {
         if(block_read(i+1, &fat[i]) == ERROR)
         {
+            // fail to read the block to fat
             return ERROR;
         }
     }
+    // success in mount a new file system
     return SUCCE;
 }
-
 int fs_umount(void)
 {
 	/* TODO: Phase 1 */
+    //error checking
 	if(block_disk_count() == ERROR)
 	{
+        // check if there is disk opened
 		return ERROR;
 	}
 	if(block_disk_close() == ERROR)
 	{
         return ERROR;
     }
-
+    //unmount the file system
     free(fat);
-    
 	return SUCCE;
 }
 
 int fs_info(void)
 {
     /* TODO: Phase 1 */
+    //error checking
     if(block_disk_count() == ERROR)
     {
+        // check if there is disk opened
         return ERROR;
     }
-
+    // print the current information of the file system
 	printf("FS Info:\n");
     printf("total_blk_count=%u\n", super.total_blocks);
     printf("fat_blk_count=%u\n", super.num_blocks_fat);
     printf("rdir_blk=%u\n", super.root_dir_index);
     printf("data_blk=%u\n", super.data_block_start_index);
     printf("data_blk_count=%u\n", super.amount_data_blocks);
-
+    // count the blocks that are fat free
     int fat_free_blocks = 0;
     for (int i = 0; i < super.amount_data_blocks; i++) 
     {
@@ -117,8 +121,9 @@ int fs_info(void)
             fat_free_blocks++;
         }
     }
+    // fat_free_ratio is the ratio of blocks that are fat free to the total number of blocks
     printf("fat_free_ratio=%d/%u\n", fat_free_blocks, super.amount_data_blocks);
-
+    // count the number of root directory that are free
     int rdirCount = 0;
     for(int i = 0; i < FS_FILE_MAX_COUNT; i++)
     {
@@ -128,79 +133,82 @@ int fs_info(void)
         }
     }
     printf("rdir_free_ratio=%d/%d\n", rdirCount, FS_FILE_MAX_COUNT);
-    
+    //done
 	return SUCCE;
 }
-
 int fs_create(const char *filename)
 {
 	/* TODO: Phase 2 */
+    // error checking for the parameter
 	if(filename == NULL || strlen(filename) >= FS_FILENAME_LEN)
 	{
-        printf("he 1");
+        // invlid parameter
 		return ERROR;
 	}
+    // check is the given filename exist
 	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) 
 	{
         if (strcmp(rootDir[i].filename, filename) == 0) 
 		{
-            printf("he 2");
+            // given filename exist, can't create
             return ERROR; 
         }
     }
+    // get a empty directory to store the new file
 	int empty_entry = ERROR;
     for (int i = 0; i < FS_FILE_MAX_COUNT; i++) 
 	{
         if (strcmp(rootDir[i].filename, "") == 0) 
 		{
+            // empty space found
             empty_entry = i;
             break;
         }
     }
 	if (empty_entry == ERROR) 
 	{
-        printf("he 4");
+        // there are no more empty space to create a new file
         return ERROR;
     }
-
-	memset(rootDir[empty_entry].filename, 0, FS_FILENAME_LEN); // Clear filename buffer
+    // initialize the directory 
+	memset(rootDir[empty_entry].filename, 0, FS_FILENAME_LEN);
     strncpy(rootDir[empty_entry].filename, filename, FS_FILENAME_LEN - 1);
     rootDir[empty_entry].size_of_file = 0; 
     rootDir[empty_entry].index_of_first = FAT_EOC;
 
     if (block_write(super.root_dir_index, rootDir) == ERROR)
     {
+        // fail in writing the block to disk
         return ERROR;
     }
-    printf("he 3");
+    // done
 	return SUCCE;
-
 }
-
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
-    printf("he 11");
 	if (filename == NULL) 
     {
-        printf("he 5");
+        // parameter has error 
         return ERROR;
     }
+    // find the index of the file in the root directory
     int file_index = ERROR;
     for (int i = 0; i < FS_FILE_MAX_COUNT; i++) 
     {
         if (strcmp(rootDir[i].filename, filename) == 0) 
         {
+            // found match file for delete
             file_index = i;
             break;
         }
     }
     if (file_index == ERROR) 
     {
-        printf("he 6");
+        // did not found the file that match the parameter
         return ERROR;
     }
-
+    // free up the data blocks allocated to the file
     uint16_t current_block = rootDir[file_index].index_of_first;
     while (current_block != FAT_EOC) 
     {
@@ -208,28 +216,31 @@ int fs_delete(const char *filename)
         fat[current_block] = 0;
         current_block = next_block;
     }
-
+    // set the value of the file as deleted in the root directory
     rootDir[file_index].filename[0] = '\0';
     rootDir[file_index].size_of_file = 0;
     rootDir[file_index].index_of_first = FAT_EOC;
-    printf("he 7");
+    // done
     return SUCCE;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
-    printf("he 8");
 	if(block_disk_count() == ERROR) 
 	{
-        printf("he 9");
+        // no block is opened in this disk
 		return ERROR;
 	}
+    // print the information 
 	printf("FS Ls:\n");
+    // iterate through the entries in the root directory
     for (int i = 0; i < FS_FILE_MAX_COUNT; i++) 
     {
+        // check if the directory entry is not empty
         if (rootDir[i].filename[0] != '\0') 
         {
+            // print information about the current file
             printf("File: %s, Size: %u, Blocks: ", rootDir[i].filename, rootDir[i].size_of_file);
             uint16_t current_block = rootDir[i].index_of_first;
             while (current_block != FAT_EOC) 
@@ -240,82 +251,85 @@ int fs_ls(void)
             printf("\n");
         }
     }
-    printf("he 10");
+    // done
     return SUCCE;
 }
 
 int fs_open(const char *filename)
 {
+    // error checking
     if (filename == NULL || strlen(filename) >= FS_FILENAME_LEN)
     {
+        // filename is invalid 
         return ERROR;
     }
-
+    // check if there is a entry matches the provided filename
     int index = ERROR;
     for (int i = 0; i < FS_FILE_MAX_COUNT; i++) 
     {
         if (strcmp(rootDir[i].filename, filename) == 0) 
         {
+            // fount the entry that need to open
             index = i; 
             break;
         }
     }
-
     if (index == ERROR) 
     {
+        // did not found the file
         return ERROR; 
     }
-
-    
+    // make sure the index is within the valid range 
     if (index < 0 || index >= FS_OPEN_MAX_COUNT) 
     {
-
         return ERROR;
     }
-
+    // set the file descriptor entry with the provided information
     strcpy(filed[index].filename, filename); 
     filed[index].offset = 0;
     filed[index].file_index = index;
-
-    printf("here 1\n");
+    //done
     return SUCCE; 
-
 }
-
 int fs_close(int fd)
 {
 	/* TODO: Phase 3 */
+    //error checking
     if (fd >= FS_OPEN_MAX_COUNT || fd < 0)
     {
+        //fd is not within the range
         return ERROR;
     }
     if (filed[fd].file_index == ERROR) 
     {
+        // the target file is not valid 
         return ERROR;
     }
+    // clear the file descriptor entry
     filed[fd].filename[0] = '\0';
     filed[fd].file_index = ERROR;
     filed[fd].offset = 0;
+    // done
     return SUCCE;
 }
-
-
 int fs_stat(int fd)
 {
 	/* TODO: Phase 3 */
+    //error checking
 	if (super.signature[0] == '\0')
     {
+        // filesystem is not mounted
         return ERROR;
     }
-    
     if (fd < 0 || fd >= FS_OPEN_MAX_COUNT || filed[fd].file_index == ERROR)
     {
+        // fd is not within the range or target file is invlid
         return ERROR;
     }
-
     int block_count = block_disk_count();
     if (block_count == ERROR)
     {
+        // invlid counts of the block
         return ERROR;
     }
 
